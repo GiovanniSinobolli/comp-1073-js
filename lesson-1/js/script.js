edc3f67a7ecd172aaee1ba5e5788ae9b4516b79a
Giovanni Sinobolli <sinobolligiovanni45@gmail.com>
2025-05-06 11:31:08 -0400

    finished html n js part of the first class

--- lesson-1/js/script.js
+++ lesson-1/js/script.js
@@ -4,4 +4,9 @@ element.addEventListener("click", updateName);
 
 function updateName(){
 	console.log("Function is working");
+
+	let firstName = prompt("what is your name?");
+
+	element.textContent = firstName;
+
 }
